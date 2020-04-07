8a1dd6b5141b3ba631cb56b9342af1197af10546
Tahsin Ates <tahsinates@gmail.com>
2020-04-07 19:47:54 +0100

    Updating Context Model Snapshot version

--- src/Data/Migrations/20200407181906_InitialMigration.Designer.cs
+++ src/Data/Migrations/20200407181906_InitialMigration.Designer.cs
@@ -4,14 +4,16 @@ using CoreCodeCamp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoreCodeCamp.Migrations
 {
     [DbContext(typeof(CampContext))]
-    partial class CampContextModelSnapshot : ModelSnapshot
+    [Migration("20200407181906_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
